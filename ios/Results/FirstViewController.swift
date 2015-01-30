86dfdd54cbc4f35cd82fef216e7eb5133f57c1b1
Janic Duplessis <janicduplessis@gmail.com>
2015-01-30 18:26:08 -0500

    start ios app

---
+++ ios/Results/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Results
+//
+//  Created by Janic Duplessis on 2015-01-30.
+//  Copyright (c) 2015 Janic Duplessis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
