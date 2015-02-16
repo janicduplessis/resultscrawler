0298b34d89ad04467cee0c119193141aec2c6963
Charles Vinette <vinettecharles@gmail.com>
2015-02-16 15:01:08 -0500

    Alamo installed for networking

--- ios/Pods/Target Support Files/Pods/Pods-environment.h
+++ ios/Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,9 +6,15 @@
 // project.
 
 
-// ChameleonFramework
-#define COCOAPODS_POD_AVAILABLE_ChameleonFramework
-#define COCOAPODS_VERSION_MAJOR_ChameleonFramework 1
-#define COCOAPODS_VERSION_MINOR_ChameleonFramework 1
-#define COCOAPODS_VERSION_PATCH_ChameleonFramework 2
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 1
+#define COCOAPODS_VERSION_PATCH_Alamofire 4
+
+// Chameleonframework
+#define COCOAPODS_POD_AVAILABLE_Chameleonframework
+#define COCOAPODS_VERSION_MAJOR_Chameleonframework 1
+#define COCOAPODS_VERSION_MINOR_Chameleonframework 1
+#define COCOAPODS_VERSION_PATCH_Chameleonframework 2
 
